20fb1e5a7f8d97a9c724ec367cb32df208e0187f
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-06-24 13:58:38 -0400

    Updates

---
+++ precache-manifest.9a39d901080fc2cdccd09f420bd09dc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "edcfa8a852f959e2feef",
+    "url": "/Weather-App/static/js/runtime~main.edcfa8a8.js"
+  },
+  {
+    "revision": "c0c6d224c3108c9d0b57",
+    "url": "/Weather-App/static/js/main.c0c6d224.chunk.js"
+  },
+  {
+    "revision": "30c200fd993db5e28d59",
+    "url": "/Weather-App/static/js/1.30c200fd.chunk.js"
+  },
+  {
+    "revision": "c0c6d224c3108c9d0b57",
+    "url": "/Weather-App/static/css/main.0ab84f6c.chunk.css"
+  },
+  {
+    "revision": "58b83420776ac3ae9b9c5f4ffe5d22a9",
+    "url": "/Weather-App/index.html"
+  }
+];
